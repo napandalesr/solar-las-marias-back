3c69a71023290dece9d7f75cf6f58208475d9efa
Neider Andres <neider.pandales.ext@terpel.com>
2024-07-10 14:22:55 -0500

    Se actualizan las variables de entorno

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const port = process.env.PORT;
+const port = 4001;
 
 // Middleware para parsear el cuerpo de las solicitudes como JSON
 app.use(express.json());
@@ -13,18 +13,18 @@ app.post('/send-email', async (req, res) => {
 
   // Configura el transportador SMTP con tus credenciales de Hostinger
   let transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: process.env.SMTP_PORT == 465, // true para 465, false para otros puertos
+    host: 'smtp.hostinger.com',
+    port: 465, // o 587 si no funciona con 465
+    secure: true, // true para 465, false para otros puertos
     auth: {
-      user: process.env.SMTP_USER,
+      user: process.env.SMTP_USER, 
       pass: process.env.SMTP_PASS
     }
   });
 
   // Configura el correo electrónico
   let mailOptions = {
-    from: process.env.SMTP_HOST, // remitente
+    from: process.env.SMTP_USER, // remitente
     to: to, // destinatario
     subject: subject,
     text: text,
